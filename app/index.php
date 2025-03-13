585af00ef0d73be363ab28f58693e519fbc1c09a
dieto01 <diego.silva1225@etec.sp.gov.br>
2025-03-13 01:44:02 +0000

    e if

--- app/index.php
+++ app/index.php
@@ -1,8 +1,34 @@
+
 <?php
-// require './vendor/autoload.php';
+$nome = '';
+$periodo = '';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nome = $_POST['nome'];
+    $periodo = $_POST['periodo'];
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Olá <?= htmlspecialchars($nome) ?></h1>
+    <?php  
+    ?>
+    <form action="/index.php" method="post">
+        <label for="periodo">Selecione o período:</label>
+        <select name="periodo">
+            <option value="m">Manha</option>
+            <option value="t">arde</option>
+            <option value="n">Noite</option>
+        </select>
 
-// use Application\DBConnection\MySQLConnection;
+        <label for="nome">Digite o seu nome:</label>
+        <input type="text" name="nome">
+        <button type="submit">Enviar</button> 
+    </form>
+</body>
+</html>
 
-// $db = new MySQLConnection();
-
-echo phpinfo();
